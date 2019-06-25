79013cc8ed03c1577c2cb936c757679ce1f5fad7
rpstro02 <email>
2019-06-25 17:18:27 -0400

    Updated to work with UIPanGestureRecognizer

--- Pod/Classes/EPSignatureView.swift
+++ Pod/Classes/EPSignatureView.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 open class EPSignatureView: UIView {
-
+    
     // MARK: - Private Vars
     
     fileprivate var bezierPoints = [CGPoint](repeating: CGPoint(), count: 5)
@@ -20,7 +20,7 @@ open class EPSignatureView: UIView {
     
     open var strokeColor = UIColor.black
     open var strokeWidth: CGFloat = 2.0 {
-	    didSet { bezierPath.lineWidth = strokeWidth }
+        didSet { bezierPath.lineWidth = strokeWidth }
     }
     open var isSigned: Bool = false
     open var signatureGestureRecognizer:UIPanGestureRecognizer?
@@ -45,7 +45,7 @@ open class EPSignatureView: UIView {
         bezierPath.stroke()
         strokeColor.setStroke()
         bezierPath.stroke()
-
+        
     }
     
     
@@ -61,25 +61,20 @@ open class EPSignatureView: UIView {
     @objc func panGestureRecognized(_ gesture: UIPanGestureRecognizer) {
         let touchPoint = gesture.location(in: self)
         let endAngle: CGFloat = .pi * 2.0
-        bezierPath.move(to: touchPoint)
-        bezierPath.addArc(withCenter: touchPoint, radius: 2, startAngle: 0, endAngle: endAngle, clockwise: true)
-        setNeedsDisplay()
-    }
-    
-    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-        if let currentPoint = touchPoint(touches) {
+        
+        switch gesture.state {
+        case .began:
+            bezierPath.move(to: touchPoint)
+            //bezierPath.addArc(withCenter: touchPoint, radius: 2, startAngle: 0, endAngle: endAngle, clockwise: true)
             isSigned = true
-            bezierPoints[0] = currentPoint
+            bezierPoints[0] = gesture.location(in: self)
             bezierCounter = 0
-        }
-    }
-    
-    override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let currentPoint = touchPoint(touches) {
+            setNeedsDisplay()
+        case .changed:
+            let currentPoint = gesture.location(in: self)
             bezierCounter += 1
             bezierPoints[bezierCounter] = currentPoint
-
+            
             //Smoothing is done by Bezier Equations where curves are calculated based on four concurrent  points drawn
             if bezierCounter == 4 {
                 bezierPoints[3] = CGPoint(x: (bezierPoints[2].x + bezierPoints[4].x) / 2 , y: (bezierPoints[2].y + bezierPoints[4].y) / 2)
@@ -90,12 +85,16 @@ open class EPSignatureView: UIView {
                 bezierPoints[1] = bezierPoints[4]
                 bezierCounter = 1
             }
+            
+        case .cancelled, .ended, .failed:
+            bezierCounter = 0
+            
+        case .possible:
+            print("poss")
         }
+        
     }
     
-    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        bezierCounter = 0
-    }
     
     func touchPoint(_ touches: Set<UITouch>) -> CGPoint? {
         if let touch = touches.first {
@@ -107,7 +106,7 @@ open class EPSignatureView: UIView {
     //MARK: Utility Methods
     
     /** Clears the drawn paths in the canvas
-    */
+     */
     open func clear() {
         isSigned = false
         bezierPath.removeAllPoints()
@@ -138,7 +137,7 @@ open class EPSignatureView: UIView {
     
     /** Returns the rect of signature image drawn in the canvas. This can very very useful in croping out the unwanted empty areas in the signature image returned.
      */
-
+    
     open func getSignatureBoundsInCanvas() -> CGRect {
         return bezierPath.bounds
     }
@@ -150,7 +149,7 @@ open class EPSignatureView: UIView {
             NSKeyedArchiver.archiveRootObject(bezierPath, toFile: localPath)
         }
     }
-
+    
     open func loadSignature(_ filePath: String) {
         if let path = getPath(filePath) {
             isSigned = true
